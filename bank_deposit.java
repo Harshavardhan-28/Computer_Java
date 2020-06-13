697fda17005187ad26c0d0679e861c436dd2232f
Harshavardhan-28 <66818754+Harshavardhan-28@users.noreply.github.com>
2020-06-13 22:57:34 +0530

    Add files via upload

---
+++ bank_deposit.java
@@ -0,0 +1,28 @@
+class bank_deposit
+{
+    
+ void switch_case(char a,double p,double r, double n)
+ {
+     double amt;
+     switch(a)
+     {
+         case'1':
+         amt=p*Math.pow((1+(r/100)),n);
+         break;
+         case'2':
+         amt=(p*n)+(p*((n*n+n)/2)*(r/100)*(1/12));
+         break;
+         default:
+         System.out.println("Invalid input");
+         
+        }
+     
+    }
+    
+    public static void main()
+    {
+     bank_deposit obj=new bank_deposit();
+     obj.switch_case('1',8000,9,12);
+    }
+    
+}
---
+++ method_overload.java
@@ -0,0 +1,26 @@
+class method_overload
+{
+ void calc_int(int a, char ch)
+ {
+     if( ch == 'S')
+     {
+         System.out.println(a*a);
+        }
+        else
+        {
+            System.out.println(a*a*a);
+        }
+    }
+    void calc_int(double a,double b, char ch)
+    {
+     if(ch == 'P')
+     {
+         System.out.println(a*b);
+        }
+        else
+        {
+            System.out.println(a+b);
+        }
+    }
+    
+}
